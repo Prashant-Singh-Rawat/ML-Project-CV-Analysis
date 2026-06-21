60c6a5638bdd3b90379eee3226479bf7a6e5ac03
{Prashant} <{rawatnaksh67@gmail.com}>
2026-06-21 09:23:51 +0530

    Update vite.config.js base for gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ML-Project-CV-Analysis/',
   plugins: [react()],
   cacheDir: '.vite_cache',
   server: {
